eb7238b9b968185d028ae2ae022016abd690fb34
Harsh Kumar <[harshkr.221104@gmail.com]>
2026-06-22 14:57:17 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+
+
+  return (
+  <>
+  <div>
+    Hello I am now working on the frontend of the project
+  </div>
+  </>
+  )
+}
+
+export default App
