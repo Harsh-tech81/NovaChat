c6c9fa2212726575fae0df2daf922708d0089645
Harsh Kumar <[harshkr.221104@gmail.com]>
2026-06-25 00:22:18 +0530

    update chatBox.jsx

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -5,7 +5,9 @@ import Message from "./Message";
 function ChatBox() {
   const { selectedChat, theme } = useAppContext();
   const [messages, setMessages] = useState([]);
-
+  const [prompt, setPrompt] = useState("");
+  const [mode, setMode] = useState("text");
+  const [isPublished, setIsPublished] = useState(false);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -13,6 +15,10 @@ function ChatBox() {
       setMessages(selectedChat.messages);
     }
   }, [selectedChat]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40">
@@ -32,9 +38,44 @@ function ChatBox() {
         {messages.map((message, index) => (
           <Message key={index} message={message} />
         ))}
+        {/* Three dots for msg loading */}
+        {loading && (
+          <div className="flex items-center gap-1.5 loader">
+            <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
+            <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
+            <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
+          </div>
+        )}
       </div>
       {/* Prompt Input Box */}
-      <form action=""></form>
+      <form  className="bg-primary/20 dark:bg-[#583C79]/30 border border-primary dark:border-[#80609F]/30 rounded-full flex items-center w-full max-w-2xl p-3 pl-4 mx-auto gap-4 ">
+        <select
+          className="text-sm pl-3 pr-2 outline-none"
+          value={mode}
+          onChange={(e) => setMode(e.target.value)}
+        >
+          <option className="dark:bg-purple-900" value="text">
+            Text
+          </option>
+          <option className="dark:bg-purple-900" value="image">
+            Image
+          </option>
+        </select>
+        <input
+          type="text"
+          placeholder="Type your prompt here..."
+          value={prompt}
+          className="flex-1 w-full text-sm outline-none"
+          required
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+        <button disabled={loading}>
+          <img
+            src={loading ? assets.stop_icon : assets.send_icon}
+            className="w-8 cursor-pointer"
+          />
+        </button>
+      </form>
     </div>
   );
 }
